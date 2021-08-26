97e844df1f93a6bead7be4e74f560d4c2ebdffdd
ermiry <erick.salas@ermiry.com>
2021-08-26 12:41:14 -0500

    Updated version to 0.2

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 #ifndef _SERVICE_VERSION_H_
 #define _SERVICE_VERSION_H_
 
-#define SERVICE_VERSION			"0.1"
-#define SERVICE_VERSION_NAME	"Version 0.1"
-#define SERVICE_VERSION_DATE	"07/08/2021"
-#define SERVICE_VERSION_TIME	"23:13 CST"
+#define SERVICE_VERSION			"0.2"
+#define SERVICE_VERSION_NAME	"Version 0.2"
+#define SERVICE_VERSION_DATE	"26/08/2021"
+#define SERVICE_VERSION_TIME	"12:39 CST"
 #define SERVICE_VERSION_AUTHOR	"Erick Salas"
 
 // print full worker version information
